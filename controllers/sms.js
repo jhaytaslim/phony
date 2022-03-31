a8ef9b2f29b16790eb4a67fd2efa8ba1e3980063
apple <jhaytaslim@gmail.com>
2022-03-31 13:53:02 +0100

    updated sms controller

--- controllers/sms.js
+++ controllers/sms.js
@@ -1,6 +1,13 @@
 const { JsonResponse } = require('../lib/apiResponse')
 const Services = require('../services')
-const { getAsync, setAsync, get, set, getAll, client } = require('../startup/redis')
+const {
+  getAsync,
+  setAsync,
+  get,
+  set,
+  getAll,
+  client
+} = require('../startup/redis')
 const { validateInbound, requireInbound } = require('../validations/sms')
 
 module.exports = {
@@ -20,18 +27,16 @@ module.exports = {
       const services = new Services()
       // remember
       //check if account has to
-      // let phones = await services.retrievePhone(req.accountId)
+      let phones = await services.retrievePhone(req.accountId)
 
-      // if (!phones || phones.some(item => item.number === to))
-      //   return JsonResponse(res, 400, '', '"to" parameter not found')
+      if (!phones || phones.some(item => item.number === to))
+        return JsonResponse(res, 400, '', '"to" parameter not found')
 
       // check if value is STOP and cache
       if (
         req.body.text.replace(/(\r\n|\n|\r)/gm, '').toUpperCase() === 'STOP'
       ) {
-        await set({ key, value: { from, to} })
-        const getRes = await get(key)
-        console.log('done',getRes)
+        await set({ key, value: { from, to } })
       }
 
       return JsonResponse(res, 200, 'inbound sms ok')
@@ -52,21 +57,31 @@ module.exports = {
 
       //check if from/to appeared before with a STOP
       const getRes = await get(key)
-        console.log('done',getRes)
-      if(getRes) 
-      return JsonResponse(res, 200, null, `sms from ${from} to ${to} blocked by STOP request`)
+      console.log('done', getRes)
+      if (getRes)
+        return JsonResponse(
+          res,
+          200,
+          null,
+          `sms from ${from} to ${to} blocked by STOP request`
+        )
 
       const services = new Services()
       //check if account has to
       let phones = await services.retrievePhone(req.accountId)
 
-      // if (!phones || phones.some(item => item.number === from))
-      //   return JsonResponse(res, 400, '', '"from" parameter not found')
+      if (!phones || phones.some(item => item.number === from))
+        return JsonResponse(res, 400, '', '"from" parameter not found')
 
+      //if >= 50 outbound requests from "from" cry
+      //return JsonResponse(res, 400, '', 'limit reached for from "from"')
 
-        //if >= 50 outbound requests from "from" cry
-        //return JsonResponse(res, 400, '', 'limit reached for from "from"')
-
+      // check if value is STOP and cache
+      if (
+        req.body.text.replace(/(\r\n|\n|\r)/gm, '').toUpperCase() === 'STOP'
+      ) {
+        await set({ key, value: { from, to } })
+      }
 
       return JsonResponse(res, 200, 'outbound sms ok')
     } catch (err) {
